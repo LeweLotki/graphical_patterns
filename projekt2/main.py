c4090379ccf939b9f977e72aa0f9680e07d29c64
LeweLotki <68538575+LeweLotki@users.noreply.github.com>
2020-11-03 20:57:26 +0100

    Update main.py

--- projekt2/main.py
+++ projekt2/main.py
@@ -13,6 +13,9 @@ def draw_rectangle(event, x, y, flags, param):
       
     global ix, iy, drawing, img 
       
+    cv2.circle(img, (90,im.square_cord+30), 42, (215,215,215), -1)
+    img[im.square_cord:im.square_cord+im.size[0], im.size[0]:2*im.size[0], :] = im.square_shaded    
+      
     if event == cv2.EVENT_LBUTTONDOWN: 
         drawing = True
         ix = x 
@@ -39,6 +42,9 @@ def rubber(event, x, y, flags, param):
       
     global ix, iy, drawing, img 
       
+    cv2.circle(img, (90,im.rubber_cord+30), 42, (215,215,215), -1)
+    img[im.rubber_cord:im.rubber_cord+im.size[0], im.size[0]:2*im.size[0], :] = im.rubber_shaded  
+      
     if event == cv2.EVENT_LBUTTONDOWN: 
         drawing = True
         ix = x 
@@ -60,6 +66,9 @@ def rubber(event, x, y, flags, param):
 def pencil(event, x, y, flags, param): 
       
     global ix, iy, drawing, img 
+    
+    cv2.circle(img, (90,im.pencil_cord+30), 42, (215,215,215), -1)
+    img[im.pencil_cord:im.pencil_cord+im.size[0], im.size[0]:2*im.size[0], :] = im.pencil_shaded
       
     if event == cv2.EVENT_LBUTTONDOWN: 
         drawing = True
@@ -82,7 +91,10 @@ def pencil(event, x, y, flags, param):
 def circle(event, x, y, flags, param): 
       
     global ix, iy, drawing, img 
-      
+    
+    cv2.circle(img, (90,im.circle_cord+30), 42, (215,215,215), -1)
+    img[im.circle_cord:im.circle_cord+im.size[0], im.size[0]:2*im.size[0], :] = im.circle_shaded
+    
     if event == cv2.EVENT_LBUTTONDOWN: 
         drawing = True
         ix = x 
@@ -104,6 +116,34 @@ def circle(event, x, y, flags, param):
 def line(event, x, y, flags, param): 
       
     global ix, iy, drawing, img 
+    
+    cv2.circle(img, (90,im.line_cord+30), 42, (215,215,215), -1)
+    img[im.line_cord:im.line_cord+im.size[0], im.size[0]:2*im.size[0], :] = im.line_shaded
+    
+    if event == cv2.EVENT_LBUTTONDOWN: 
+        drawing = True
+        ix = x 
+        iy = y             
+        if x > 70 and x < 140: 
+            cv2.setMouseCallback("image", activate) 
+            
+    # elif event == cv2.EVENT_MOUSEMOVE: 
+        # if drawing == True:
+            # # print(ix,iy)
+            # if iy > 50 and iy < 700 and ix > 250 and ix < 900 and y > 50 and y < 700 and x > 250 and x < 900 :
+                # cv2.line(img,(ix,iy),(x,y),(0,0,0),2)
+      
+    elif event == cv2.EVENT_LBUTTONUP: 
+        drawing = False
+        if iy > 50 and iy < 700 and ix > 250 and ix < 900 and y > 50 and y < 700 and x > 250 and x < 900:
+            cv2.line(img,(ix,iy),(x,y),(0,0,0),2)
+
+def plume(event, x, y, flags, param): 
+      
+    global ix, iy, drawing, img 
+    
+    cv2.circle(img, (90,im.plume_cord+30), 42, (215,215,215), -1)
+    img[im.plume_cord:im.plume_cord+im.size[0], im.size[0]:2*im.size[0], :] = im.plume_shaded    
       
     if event == cv2.EVENT_LBUTTONDOWN: 
         drawing = True
@@ -123,21 +163,25 @@ def line(event, x, y, flags, param):
         if iy > 50 and iy < 700 and ix > 250 and ix < 900 and y > 50 and y < 700 and x > 250 and x < 900:
             cv2.line(img,(ix,iy),(x,y),(0,0,0),2)
 
-def activate(event, x, y, flags, param):        
-
-    if event == cv2.EVENT_LBUTTONDOWN and y > 635 and y < 705 and x > 70 and x < 140: 
+def activate(event, x, y, flags, param):   
+    
+    for val in im.list_icon:
+        cv2.circle(img, (90,val[1]+30), 42, (255,255,255), -1)
+        img[val[1]:val[1]+im.size[0], im.size[0]:2*im.size[0], :] = val[0]
+    
+    if event == cv2.EVENT_LBUTTONDOWN and y > im.square_cord and y < im.square_cord+70 and x > 70 and x < 140: 
         cv2.setMouseCallback("image", draw_rectangle)
         
-    if event == cv2.EVENT_LBUTTONDOWN and y > 535 and y < 605 and x > 70 and x < 140:
+    if event == cv2.EVENT_LBUTTONDOWN and y > im.rubber_cord and y < im.rubber_cord+70 and x > 70 and x < 140:
         cv2.setMouseCallback("image", rubber)
     
-    if event == cv2.EVENT_LBUTTONDOWN and y > 435 and y < 505 and x > 70 and x < 140:
+    if event == cv2.EVENT_LBUTTONDOWN and y > im.pencil_cord and y < im.pencil_cord+70 and x > 70 and x < 140:
         cv2.setMouseCallback("image", pencil)  
     
-    if event == cv2.EVENT_LBUTTONDOWN and y > 135 and y < 405 and x > 70 and x < 140:
+    if event == cv2.EVENT_LBUTTONDOWN and y > im.circle_cord and y < im.circle_cord+70 and x > 70 and x < 140:
         cv2.setMouseCallback("image", circle)  
         
-    if event == cv2.EVENT_LBUTTONDOWN and y > 235 and y < 305 and x > 70 and x < 140:
+    if event == cv2.EVENT_LBUTTONDOWN and y > im.line_cord and y < im.line_cord+70 and x > 70 and x < 140:
         cv2.setMouseCallback("image", line)  
 
 img[50:700,250:900,:] = (255, 255, 255)
