8c929d3737ab6f2dada057fb524a1e5e925b7e19
LeweLotki <68538575+LeweLotki@users.noreply.github.com>
2020-11-03 20:57:05 +0100

    Update image.py

--- projekt2/packages/image.py
+++ projekt2/packages/image.py
@@ -8,6 +8,7 @@ paint = cv2.imread(r'F:\freelancer\projekt2\images\icons\ronded\cpaint.png')
 pencil = cv2.imread(r'F:\freelancer\projekt2\images\icons\ronded\cpencil.png')
 rubber = cv2.imread(r'F:\freelancer\projekt2\images\icons\ronded\crubber.png')
 square = cv2.imread(r'F:\freelancer\projekt2\images\icons\ronded\csquare.png')
+
 fill_cord = 35
 circle_cord = fill_cord + 100
 line_cord = circle_cord + 100
@@ -17,10 +18,18 @@ rubber_cord = pencil_cord + 100
 square_cord = rubber_cord + 100
 size = (60,60)
 list_icon = [[fill,fill_cord], [circle, circle_cord] ,[line, line_cord] , [paint, paint_cord], [pencil, pencil_cord], [rubber, rubber_cord], [square, square_cord]]
+# list_icon_shaded = [[fill_shaded,fill_cord], [circle_shaded, circle_cord] ,[line_shaded, line_cord] , [paint_shaded, paint_cord], [pencil_shaded, pencil_cord], [rubber_shaded, rubber_cord], [square_shaded, square_cord]]
+
 for val in list_icon:
    val[0] = cv2.resize(val[0],size) 
 
-
+fill_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\sfill.png'),size)
+circle_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\scircle.png'),size) 
+line_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\sline.png'),size) 
+paint_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\spaint.png'),size) 
+pencil_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\spencil.png'),size)
+rubber_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\srubber.png'),size) 
+square_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\ssquare.png'),size) 
 
 
 
