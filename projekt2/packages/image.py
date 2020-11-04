0f9cd65e73da5a11774cf7e63ffacd61986a1b13
LeweLotki <68538575+LeweLotki@users.noreply.github.com>
2020-11-04 19:19:14 +0100

    Update image.py

--- projekt2/packages/image.py
+++ projekt2/packages/image.py
@@ -17,6 +17,7 @@ pencil_cord = paint_cord + 100
 rubber_cord = pencil_cord + 100
 square_cord = rubber_cord + 100
 size = (60,60)
+size_brush = (20,20)
 list_icon = [[fill,fill_cord], [circle, circle_cord] ,[line, line_cord] , [paint, paint_cord], [pencil, pencil_cord], [rubber, rubber_cord], [square, square_cord]]
 # list_icon_shaded = [[fill_shaded,fill_cord], [circle_shaded, circle_cord] ,[line_shaded, line_cord] , [paint_shaded, paint_cord], [pencil_shaded, pencil_cord], [rubber_shaded, rubber_cord], [square_shaded, square_cord]]
 
@@ -31,5 +32,16 @@ pencil_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shad
 rubber_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\srubber.png'),size) 
 square_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\shaded\ssquare.png'),size) 
 
+ths = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\ths.png'),size_brush)
+thn = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\thn.png'),size_brush)
+thb = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\thb.png'),size_brush)
 
+ths_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\ths_shaded.png'),size_brush)
+thn_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\thn_shaded.png'),size_brush)
+thb_shaded = cv2.resize(cv2.imread(r'F:\freelancer\projekt2\images\icons\thb_shaded.png'),size_brush)
 
+ths_cord = line_cord + 20
+thn_cord = paint_cord + 20
+thb_cord = pencil_cord + 20
+
+list_brush = [[ths,ths_cord],[thn, thn_cord],[thb, thb_cord]]
